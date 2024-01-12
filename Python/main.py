fe1e0c8c69946a0615903f8dec780662676d20a7
kvoloshenko <kvoloshenko@gmail.com>
2024-01-12 20:47:50 +0300

    Initial commit

--- Python/main.py
+++ Python/main.py
@@ -15,6 +15,12 @@ ELEVENLABS_API_KEY = os.environ.get("ELEVENLABS_API_KEY")
 set_api_key(ELEVENLABS_API_KEY)
 
 def record_audio(duration=8, fs=44100):
+    """
+    Recording an audio file
+    :param duration:
+    :param fs:
+    :return:
+    """
     print('Recording...')
     myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
     sd.wait()
@@ -24,6 +30,13 @@ def record_audio(duration=8, fs=44100):
 
 
 def chat(host, system_content, user_content):
+    """
+    Sending data via POST request to a proxy server for ChatGPT
+    :param host:
+    :param system_content:
+    :param user_content:
+    :return:
+    """
     end_point = "/chat"
     url = host + end_point
     print(f'url={url}')
@@ -39,6 +52,11 @@ def chat(host, system_content, user_content):
     return answer
 
 def transcript(host):
+    """
+    Sending the audio file POST request to a proxy server for transcription
+    :param host:
+    :return:
+    """
     end_point = "/transcript"
     url = host + end_point
     print(f'url={url}')
@@ -54,6 +72,11 @@ def transcript(host):
     return transcript
 
 def  text_2_speech(text):
+    """
+    Using lElevenLabs for converting text to speech
+    :param text:
+    :return:
+    """
     # https://github.com/elevenlabs/elevenlabs-python
     # https://github.com/BtbN/FFmpeg-Builds/releases
     audio = generate(
@@ -65,7 +88,7 @@ def  text_2_speech(text):
 
 
 if __name__ == "__main__":
-    host = 'https://dad3-34-106-201-153.ngrok-free.app'  # эту ссылку надо заменить на актуальную из Colab
+    host = 'https://dad3-34-106-201-153.ngrok-free.app'  # this link should be replaced with the current one from Colab
 
     system_content = """ You're my English teacher and conversation partner that helps me practice speaking English in a casual manner. 
     Speak to me only in English. 
@@ -75,12 +98,12 @@ if __name__ == "__main__":
     Gently correct major grammatical errors with helpful advice. Not directly, but by making suggestions on how to phrase things more naturally. 
     Start with the simplest level possible, assuming the user is at the “Pre-intermediate” level. 
     Based on their responses, adapt the level of language in a conversations to match the user’s level. 
-    Don’t acknowledge the adaptation, behave naturally. """  # Текст промпта
+    Don’t acknowledge the adaptation, behave naturally. """  # Prompt text
 
     while True:
-        record_audio() # создается аудиофайл myrecording.wav
-        user_content = transcript(host)
+        record_audio()                                    # Creating 'myrecording.wav' audio file
+        user_content = transcript(host)                   # Sending the audio file for transcription
         print(f'user_content={user_content}')
-        answer = chat(host, system_content, user_content)
+        answer = chat(host, system_content, user_content) # Sending data for ChatGPT
         print(answer)
-        text_2_speech(answer)
+        text_2_speech(answer)                             # Converting the answer to speech
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # talk-to-chatgpt
 Talk To ChatGPT
+
+
+## llElevenLabs: https://elevenlabs.io/
+
+voice: "Victoria - classy and mature"
+
+## Flask Uploading Files: 
+https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
+
+## FFmpeg
+https://ffmpeg.org/
+
+https://github.com/BtbN/FFmpeg-Builds/releases
